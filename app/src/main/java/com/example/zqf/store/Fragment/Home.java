37139d098ef20a16d99a410d4037534ef98b9184
JH <1162634877@qq.com>
2018-02-24 23:12:49 +0800

    江浩新界面两个

--- app/src/main/java/com/example/zqf/store/Fragment/Home.java
+++ app/src/main/java/com/example/zqf/store/Fragment/Home.java
@@ -2,11 +2,16 @@ package com.example.zqf.store.Fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.example.zqf.store.Adapter.HomePageAdapter;
 import com.example.zqf.store.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by admin on 2018/2/12.
@@ -14,10 +19,23 @@ import com.example.zqf.store.R;
 
 public class Home extends Fragment {
     Button but1,but2,but3,but4,but5,but6,but7;
+    HomePageAdapter mAdapter;
+    ArrayList<View> aList;
+    ViewPager vpager_one;
+
     public Home (){}
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_home, container, false);
+        vpager_one = view.findViewById(R.id.mPager);
+
+        aList = new ArrayList<View>();
+        aList.add(inflater.inflate(R.layout.view_one,null,false));
+        aList.add(inflater.inflate(R.layout.view_two,null,false));
+        aList.add(inflater.inflate(R.layout.view_three,null,false));
+        mAdapter = new HomePageAdapter(aList);
+        vpager_one.setAdapter(mAdapter);
+
         but1=view.findViewById(R.id.button);
         but2=view.findViewById(R.id.button2);
         but3=view.findViewById(R.id.button3);
--- app/src/main/java/com/example/zqf/store/Fragment/My.java
+++ app/src/main/java/com/example/zqf/store/Fragment/My.java
@@ -1,11 +1,22 @@
 package com.example.zqf.store.Fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.zqf.store.R;
 
 /**
  * Created by admin on 2018/2/12.
  */
 
 public class My extends Fragment {
-
+    public My (){}
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_my, container, false);
+        return view;
+    }
 }
